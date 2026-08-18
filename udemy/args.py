ea8e45dfa99b36d2db39da5ef5b1ef5e8826f0ef
italo aquino <italo@aquino.localdomain>
2026-08-18 11:15:15 -0300

    feat:Criação de exericios pra treinar args

---
+++ udemy/args.py
@@ -0,0 +1,11 @@
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+digitado  = input("Digite numeros: ")   
+partes  = digitado.split()              
+numeros   = [int(_) for _ in partes]    
+resultado = soma(*numeros)              
+print(resultado)
---
+++ udemy/args2.py
@@ -0,0 +1,25 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def multiplicacao(*args):
+    total = 1
+    for numero in args:
+        total = numero * total
+    return total
+
+numeros_digitados = input("Digite um numero: ")
+partes = numeros_digitados.split()
+formato =  [int(_) for _ in partes]
+resultadototal = multiplicacao(*formato)
+print(resultadototal)
+
+if resultadototal % 2 == 0:
+    print("seu numero é par")
+else : print("seu numero é impar")
